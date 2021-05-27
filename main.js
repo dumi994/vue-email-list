c58ac32174671d21606eb5ddb222c61ce4065ba6
dumi994 <dumi.9944@gmail.com>
2021-05-27 16:33:05 +0200

    la console stampa 10 mail diverse

--- main.js
+++ main.js
@@ -1,16 +1,24 @@
 const app = new Vue ({
     el: '#app',
     data:{
-        randomMail:'',
+        randomMail:[],
     },
     methods:{
 
     },
     mounted(){
-        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+        /* axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
         .then(response =>{
             this.randomMail = response.data.response;
             console.log(this.randomMail);
-        })
+        }) */
+        for (let i = 0; i < 10; i++){
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then(response =>{
+                this.randomMail = response.data.response;
+                console.log(this.randomMail);
+            })
+        }
+     
     }
 })
